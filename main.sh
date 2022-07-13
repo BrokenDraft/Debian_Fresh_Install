8881c09e1cf1be08f2e29027dcab6a4a7dfee888
BrokenDraft <42341867+BrokenDraft@users.noreply.github.com>
2022-07-13 18:01:05 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,5 @@
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/BrokenDraft/Oh-my-zsh_deb_Full-install/main/good_boi.sh)"
 sudo apt install python3
 sudo apt install tmux
 git clone https://github.com/BrokenDraft/nanorc-file.git
 cd nanorc-file ; sudo ./add_nanorc.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/BrokenDraft/Oh-my-zsh_deb_Full-install/main/good_boi.sh)"
